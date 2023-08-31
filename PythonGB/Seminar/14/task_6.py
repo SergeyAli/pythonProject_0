475ec309b472e51026d5b39b10221d75813d9d64
ASN <aliger99@rambler.ru>
2023-08-31 20:53:18 +0700

    Seminar_14_0

--- PythonGB/Seminar/14/task_6.py
+++ PythonGB/Seminar/14/task_6.py
@@ -5,4 +5,33 @@
 📌 Используйте фикстуры.
 '''
 
+import pytest                      # необходимо установить в окружение
 
+
+from Semin13_Task5 import UserWorkshop, User
+from Exceptions import AccesErorr, LevelError
+from Semin13_User import User
+@pytest.fixture()
+def set_up():
+    return UserWorkshop()   # данные для теста
+
+
+def test_access_fail_1(set_up):                               # тест на проверку того, что  будет выброс исключения AccesErorr при вводе невалидных данных, AccesErorr  прописан в Exceptions.py
+
+    with pytest.raises(AccesErorr, match='Access denied'):
+        set_up.login('Nesterovs', '1')                      # передаются невалидные данные
+
+
+def test_access(set_up):                                    # тест на проверку валидных данных, то что должна вернуться
+    assert set_up.login('Nesterov', '1') == '5'             # вводятся валидные двнные и проверяем на совпадение с результатом
+
+
+def test_access_fail_2(set_up):                                    # тест на проверку того, что  будет выброс исключения AccesErorr при вводе невалидных данных, AccesErorr  прописан в Exceptions.py
+    with pytest.raises(LevelError):
+        set_up.login('Nesterov', '1')
+        set_up.create_user('New_user', '1', '3')
+
+
+
+if __name__ == '__main__':
+    pytest.main(['-v'])
